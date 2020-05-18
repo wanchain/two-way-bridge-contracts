d59d1654334013e549f8a3a37a9320cc9ff4f9c8
zhaoxiaofeng <zhao.xiaofeng@wanglutech.com>
2020-05-18 19:17:40 +0800

    chane bytes to bytes32

--- contracts/interfaces/IMetric.sol
+++ contracts/interfaces/IMetric.sol
@@ -28,9 +28,9 @@ pragma solidity ^0.4.24;
 
 interface IMetric {
     // index of array: smIndex
-    function getPrdInctMetric(bytes grpId, uint startEpId, uint endEpId) external returns(uint[]);
-    function getPrdSlshMetric(bytes grpId, uint startEpId, uint endEpId) external returns(uint[]);
+    function getPrdInctMetric(bytes32 grpId, uint startEpId, uint endEpId) external returns(uint[]);
+    function getPrdSlshMetric(bytes32 grpId, uint startEpId, uint endEpId) external returns(uint[]);
 
-    function getSmSuccCntByEpId(bytes grpId, uint epId, uint8 smIndex) external returns(uint);
-    function getSlshCntByEpId(bytes grpId, uint epId, uint8 smIndex) external returns(uint);
+    function getSmSuccCntByEpId(bytes32 grpId, uint epId, uint8 smIndex) external returns(uint);
+    function getSlshCntByEpId(bytes32 grpId, uint epId, uint8 smIndex) external returns(uint);
 }
