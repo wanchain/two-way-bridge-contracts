8c86a1109280492abcc23304c3e7072bbbbbd3ef
Zhang Lihua <zhanglihua@wanchain.org>
2020-05-19 17:11:52 +0800

    udpate testcase

--- test/t.js
+++ test/t.js
@@ -2,7 +2,7 @@ const utils = require("./utils");
 
 function main() {
 
-    for(let i=2000; i<2004; i++){
+    for(let i=2000; i<2008; i++){
         let n = utils.getAddressFromInt(i)
         console.log(n.addr, n.priv.toString('hex'))
     }
