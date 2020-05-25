f84e4f6d6da4c5d7f73c58ff4818cc24e7b455f0
zhaoxiaofeng <zhao.xiaofeng@wanglutech.com>
2020-05-25 18:31:19 +0800

    change copy right from 2019 to 2020

--- contracts/metric/MetricStorage.sol
+++ contracts/metric/MetricStorage.sol
@@ -1,6 +1,6 @@
 /*
 
-  Copyright 2019 Wanchain Foundation.
+  Copyright 2020 Wanchain Foundation.
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
