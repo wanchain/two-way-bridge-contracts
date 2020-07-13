6bfb0ce973f8ce980ab3e5f03e15105c544d6b67
Grant Zhang <zhwir@163.com>
2020-07-13 12:32:39 +0800

    update gpk interface

--- contracts/interfaces/IGPK.sol
+++ contracts/interfaces/IGPK.sol
@@ -27,9 +27,6 @@
 pragma solidity ^0.4.24;
 
 interface IGPK {
-    function getPkShare(bytes32 groupId, uint index) external view returns(bytes pkShare);
-    function getGpk(bytes32 groupId) external view returns(bytes gpk);
-    function getCurveNumber() external returns(uint number);
-    function getGpk(bytes32 groupId, uint curveIndex) external view returns(bytes gpk);
-    function getPkShare(bytes32 groupId, uint smgIndex, uint curveIndex) external view returns(bytes pkShare);
+    function getPkShare(bytes32 groupId, uint index) external view returns(bytes pkShare1, bytes pkShare2);
+    function getGpk(bytes32 groupId) external view returns(bytes gpk1, bytes gpk2);
 }
