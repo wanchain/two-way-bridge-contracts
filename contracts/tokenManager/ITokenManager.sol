e705dfc0e2faeede8663096f55efb5e5abb5ce1d
codeoneline <mutouji1@163.com>
2020-07-13 14:38:13 +0800

    add interface

--- contracts/tokenManager/ITokenManager.sol
+++ contracts/tokenManager/ITokenManager.sol
@@ -1,4 +1,5 @@
 pragma solidity 0.4.26;
+pragma experimental ABIEncoderV2;
 
 interface ITokenManager {
   function getTokenPairInfo(uint id) external view
@@ -10,5 +11,8 @@ interface ITokenManager {
   function getTokenPairs() external view
     returns (uint[] id, uint[] fromChainID, bytes32[] fromAccount, uint[] toChainID, address[] tokenAddress, string[] ancestorSymbol, uint8[] ancestorDecimals);
   function getTokenPairsByChainID(uint chainID1, uint chainID2) external view
-    returns (uint[] id, uint[] fromChainID, bytes32[] fromAccount, uint[] toChainID, address[] tokenAddress, string[] ancestorSymbol, uint8[] ancestorDecimals)
+    returns (uint[] id, uint[] fromChainID, bytes32[] fromAccount, uint[] toChainID, address[] tokenAddress, string[] ancestorSymbol, uint8[] ancestorDecimals);
+
+  function mintToken(uint id, address to, uint value) external;
+  function burnToken(uint id, uint value) external;
 }
