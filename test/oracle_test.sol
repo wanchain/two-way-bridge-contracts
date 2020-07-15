4760d15168485ad4ff53ec7072bc17c8bd9005fb
codeoneline <mutouji1@163.com>
2020-07-15 12:23:10 +0800

    remove addWhiteist

--- test/oracle_test.sol
+++ test/oracle_test.sol
@@ -16,7 +16,6 @@ contract TestOracle {
     bytes32 smgID = 0x111122223333444455556666777788889999AAAABBBBCCCCDDDDEEEEFFFFCCCC;
     uint amount = 256;
 
-    oracleDelegate.addWhitelist(this);
     oracleDelegate.updateDeposit(smgID, amount);
 
     Assert.equal(iOracle.getDeposit(smgID), amount, "getDeposit can called by a contract");
