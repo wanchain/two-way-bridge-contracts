2168c38a547b3676406fa3833bb9bcc0226e8219
Grant Zhang <zhwir@163.com>
2020-08-05 17:42:49 +0800

    add replace node

--- test/integration/replaceNode.js
+++ test/integration/replaceNode.js
@@ -1,6 +1,6 @@
 const StoremanGroupProxy = artifacts.require('StoremanGroupProxy');
 const StoremanGroupDelegate = artifacts.require('StoremanGroupDelegate');
-const { registerStart, stakeInPre, toSelect } = require('../base.js')
+const { registerStart, stakeInOne } = require('../base.js')
 
 let leader = '0x5793e629c061e7fd642ab6a1b4d552cec0e2d606';
 
@@ -20,6 +20,10 @@ contract('open_storeman_it', async () => {
     sk = await smgSc.getStoremanInfo(leader);
     console.log("leader new sk: %O", sk);
     console.log("next group id: %s", sk.nextGroupId);
+
+    let newWa = await stakeInOne(smgSc, groupId, 7, 3000);
+    sk = await smgSc.getStoremanInfo(newWa);
+    console.log("new node sk: %O", sk);
   })
 
   it('it_stub', async () => {
