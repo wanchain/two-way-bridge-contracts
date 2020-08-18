b9d55ca0da54c9aa12e06f766c180ec143088664
codeoneline <mutouji1@163.com>
2020-08-18 18:50:22 +0800

    add change owner

--- contracts/tokenManager/IMappingToken.sol
+++ contracts/tokenManager/IMappingToken.sol
@@ -28,6 +28,8 @@ pragma solidity 0.4.26;
 import '../components/StandardToken.sol';
 
 interface IMappingToken {
+    function changeOwner(address _newOwner) external;
+    function acceptOwnership() external;
     function name() external view returns (string);
     function symbol() external view returns (string);
     function decimals() external view returns (uint8);
