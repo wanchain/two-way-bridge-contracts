7562d6b27d71dddaa7d440017e52a79cd3f5a2ec
Zhang Lihua <zhanglihua@wanchain.org>
2020-08-19 10:14:53 +0800

    update testcae

--- test/afterTest.sh
+++ test/afterTest.sh
@@ -1,6 +1,5 @@
 curPath=$(dirname "$0")
 cd $curPath
 
-mv  ../contracts/htlc/lib/HTLCLib.sol.org ../contracts/htlc/lib/HTLCLib.sol
 mv  ../contracts/htlc/lib/HTLCTypes.sol.org ../contracts/htlc/lib/HTLCTypes.sol
 mv  ../contracts/tokenManager/TokenManagerStorage.sol.org ../contracts/tokenManager/TokenManagerStorage.sol
