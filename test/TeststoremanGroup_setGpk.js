45187f010a3ce97e797ee40967b8c6739eebfc3f
Zhang Lihua <zhanglihua@wanchain.org>
2020-08-21 11:32:57 +0800

    udpate contract

--- test/TeststoremanGroup_setGpk.js
+++ test/TeststoremanGroup_setGpk.js
@@ -5,12 +5,7 @@ const TestSmg = artifacts.require('TestSmg')
 const StoremanGroupDelegate = artifacts.require('StoremanGroupDelegate')
 const StoremanGroupProxy = artifacts.require('StoremanGroupProxy');
 
-
-
-/*************************************
-staker: 1000 ~ 1000+100
-delegator: stakerId*100 ~ stakerID*100+1000
- ****************************************/
+const { registerStart,stakeInPre,  toSelect, } = require('./basee.js')
 
 
 contract('TestSmg', async () => {
@@ -34,16 +29,10 @@ contract('TestSmg', async () => {
     })
     
     it('test select', async ()=>{
-        let tx = await toSelect(groupId);
+        let tx = await toSelect(smg, groupId);
         console.log("select tx:", tx)
-        await utils.waitReceipt(tx.tx)
     })
 
-    it('testSetGpk', async() => {
-        let tsmg = await TestSmg.deployed();
-        let gpk = "0x04d2386b8a684e7be9f0d911c936092035dc2b112fe8c83fb602beac098183800237d173cf0e1e5a8cbb159bcdbdfbef67e25dbcc8b852e032aa2a9d7b0fe912a4"
-        let tx =  await tsmg.testSetGpk(id, gpk, gpk)
-        console.log("tx:", tx)
-    })
+
 
 })
