bd61f2bca5dd62a2fdd998921d4e79f614aec22a
lolieatapple <lolieatapple@gmail.com>
2023-01-29 14:24:51 +0800

    update sdk to latest version

---
+++ aptos-bridge/scripts/compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+aptos move compile --package-dir ../CoinBase
+aptos move compile --package-dir ../Cross
+aptos move compile --package-dir ../Oracle
+aptos move compile --package-dir ../ResourceAccount
+aptos move compile --package-dir ../WrappedCoin
