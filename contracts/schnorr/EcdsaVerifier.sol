b1c35a9027cd1444805a4130e11e9345588d42bd
Richard <Richard@wanchain.org>
2023-08-03 15:09:49 +0800

    add deploy script and config for EVM

--- contracts/schnorr/EcdsaVerifier.sol
+++ contracts/schnorr/EcdsaVerifier.sol
@@ -27,22 +27,22 @@ contract EcdsaVerifier {
         return (recoveredAddr == expectedAddr);
     }
 
-    // Function to verify the ECDSA signature
-    function debugVerify(bytes32 signature, bytes32 groupKeyX, bytes32 groupKeyY, bytes32 r, bytes32 v, bytes32 message)
-        public
-        view
-        returns(bool, address, address, uint, bytes memory)
-    {
-        // Convert bytes32 v into uint8
-        uint8 vValue = uint8(uint256(v)) + 27;
+    // // Function to verify the ECDSA signature
+    // function debugVerify(bytes32 signature, bytes32 groupKeyX, bytes32 groupKeyY, bytes32 r, bytes32 v, bytes32 message)
+    //     public
+    //     view
+    //     returns(bool, address, address, uint, bytes memory)
+    // {
+    //     // Convert bytes32 v into uint8
+    //     uint8 vValue = uint8(uint256(v)) + 27;
 
-        // Use ecrecover to recover the public key address
-        address recoveredAddr = ecrecover(message, vValue, r, signature);
+    //     // Use ecrecover to recover the public key address
+    //     address recoveredAddr = ecrecover(message, vValue, r, signature);
 
-        // Compare the recovered address with the expected address
-        bytes memory publicKey = abi.encodePacked(groupKeyX, groupKeyY);
-        address expectedAddr = publicKeyToAddress(publicKey);
+    //     // Compare the recovered address with the expected address
+    //     bytes memory publicKey = abi.encodePacked(groupKeyX, groupKeyY);
+    //     address expectedAddr = publicKeyToAddress(publicKey);
 
-        return (recoveredAddr == expectedAddr, recoveredAddr, expectedAddr, vValue, publicKey);
-    }
+    //     return (recoveredAddr == expectedAddr, recoveredAddr, expectedAddr, vValue, publicKey);
+    // }
 }
