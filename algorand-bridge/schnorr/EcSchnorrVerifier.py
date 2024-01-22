e3da8ab548f3354c7eaa513063c5f6250a5e36a8
Richard <Richard@wanchain.org>
2024-01-22 16:38:01 +0800

    update ec schnorr

--- algorand-bridge/schnorr/EcSchnorrVerifier.py
+++ algorand-bridge/schnorr/EcSchnorrVerifier.py
@@ -4,120 +4,19 @@ import pyteal as pt
 from pyteal import *
 import beaker
 
-@pt.Subroutine(pt.TealType.uint64)
-def checkPubKey(X, Y):
-    ex = Bytes("base16","ac93e52fc9b47625298fcde0feeed213947de3fe359373556bff7c5f90be863a")
-    ey = Bytes("base16","148369bba90e4c36fe19415ff15df2942841ebcba1a063e418c08c594997975b")
-    ekhash = Bytes("base16","eff210af2729ffdd83f1abe7c02ec0f73e0157b60ecaf687bda9bfb2884a2dfc")
-    eaddr = Bytes("base16","c02Ec0F73E0157b60eCAf687BdA9bFB2884a2DFC")
-    khash = Keccak256(Concat(ex, ey))
-    addr = Extract(khash, Int(12), Int(20))
 
-    return If(pt.BytesEq(addr, eaddr),  Int(1), Int(0))
-    #return If(And(pt.BytesEq(X, ex),pt.BytesEq(Y, ey)),  Int(1), Int(0))
 
 @pt.Subroutine(pt.TealType.bytes)
 def convertToEthAddr(X, Y):
     khash = Keccak256(Concat(X, Y))
     addr = Extract(khash, Int(12), Int(20))
-    eaddr = Bytes("base16","0x7C5B88e76CF02299e01e9374Ac75C397De3e15D1")
+    # eaddr = Bytes("base16","0x7C5B88e76CF02299e01e9374Ac75C397De3e15D1")
     return addr
-    #return If(pt.BytesEq(addr, eaddr),  Int(1), Int(0))
 
 
+app = beaker.Application("EcSchnorrVerifier")
 
 
-@pt.Subroutine(pt.TealType.uint64)
-def eth_ecdsa_validate(hash_value: pt.Expr, signature: pt.Expr) -> pt.Expr:
-    """
-    Return a 1/0 for valid signature given hash
-
-    Equivalent of OpenZeppelin ECDSA.recover for long 65-byte Ethereum signatures
-    https://docs.openzeppelin.com/contracts/2.x/api/cryptography#ECDSA-recover-bytes32-bytes-
-    Short 64-byte Ethereum signatures require some changes to the code
-
-
-    [1] https://github.com/OpenZeppelin/openzeppelin-contracts/blob/5fbf494511fd522b931f7f92e2df87d671ea8b0b/contracts/utils/cryptography/ECDSA.sol#L153
-
-
-    Note: Unless compatibility with Ethereum or another system is necessary,
-    we highly recommend using ed25519_verify instead of ecdsa on Algorand
-
-    WARNING: This code has NOT been audited
-    DO NOT USE IN PRODUCTION
-    """
-
-    r = pt.Extract(signature, pt.Int(0), pt.Int(32))
-    s = pt.Extract(signature, pt.Int(32), pt.Int(32))
-
-    # The recovery ID is shifted by 27 on Ethereum
-    # For non-Ethereum signatures, remove the -27 on the line below
-    v = pt.Btoi(pt.Extract(signature, pt.Int(64), pt.Int(1))) - pt.Int(27)
-
-    pt.Assert(
-        pt.Len(signature) == pt.Int(65),
-        pt.Len(hash_value) == pt.Int(32),
-        # The following two asserts are to prevent malleability like in [1]
-        pt.BytesLe(
-            s,
-            pt.Bytes(
-                "base16",
-                "0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0",
-            ),
-        ),
-        v <= pt.Int(1),
-    ),
-    # pt.EcdsaVerify(
-    #     pt.EcdsaCurve.Secp256k1,
-    #     hash_value,
-    #     r,
-    #     s,
-    #     pt.EcdsaRecover(pt.EcdsaCurve.Secp256k1, hash_value, v, r, s),
-    # ),
-    pubkey = pt.EcdsaRecover(pt.EcdsaCurve.Secp256k1, hash_value, v, r, s)
-
-
-    return pubkey.outputReducer(
-        lambda x,y: checkPubKey(x, y)
-    )
-    
-
-
-
-"""
-This LogicSignature takes two application arguments:
-  hash, signature
-and returns the validity of the signature given the hash
-as written in OpenZeppelin https://docs.openzeppelin.com/contracts/2.x/api/cryptography#ECDSA-recover-bytes32-bytes-
-(65-byte signatures only)
-"""
-
-app = beaker.Application("EthChecker")
-
-
-@app.external
-def check_eth_sig(
-    hash: pt.abi.StaticBytes[Literal[32]],
-    signature: pt.abi.StaticBytes[Literal[65]],
-    *,
-    output: pt.abi.Uint64
-) -> pt.Expr:
-    # The lsig that will be responsible for validating the
-    # incoming signature against the incoming hash
-    # When passed to Precompile, it flags the init of the Application
-    # to prevent building approval/clear programs until the precompile is
-    # compiled so we have access to compiled information (its address for instance)
-    # pt.OpUp(mode=pt.OpUpMode.OnCall).maximize_budget(
-    #     pt.Int(5000)
-    # )
-    return pt.Seq(
-        # The precompiled lsig should have its address and binary available
-        # here so we can use it to make sure we've been called
-        # with the correct lsig
-        #pt.Assert(pt.Txn.sender() == verifier.address()),
-        OpUp(mode=pt.OpUpMode.OnCall).maximize_budget(Int(3000)),
-        output.set(eth_ecdsa_validate(hash.get(), signature.get())),
-    )
 
 # function verify(bytes32 signature, bytes32 px, bytes32 py, bytes32 e, bytes32 parity, bytes32 message)
 @app.external
@@ -144,8 +43,8 @@ def check_ecSchnorr_sig(
                 "0x0000000000000000000000000000000000000000",
             )  
 
-    esp = pt.Bytes("base16", "0x4d04dbf46179932093b7f50ac68468f73df65ed3d6c2336f1cb6bf8dd2d17e9e")
-    eep = pt.Bytes("base16", "0x4e6e6611d35e09c39e131745083c59f28798e45d5e2e0005b18a591e943bcad9")
+    # esp = pt.Bytes("base16", "0x4d04dbf46179932093b7f50ac68468f73df65ed3d6c2336f1cb6bf8dd2d17e9e")
+    # eep = pt.Bytes("base16", "0x4e6e6611d35e09c39e131745083c59f28798e45d5e2e0005b18a591e943bcad9")
         # bytes32 sp = bytes32(Q - mulmod(uint256(s), uint256(px), Q));
         # bytes32 ep = bytes32(Q - mulmod(uint256(e), uint256(px), Q));
     sp = BytesMinus(Q, BytesMod(BytesMul(signature.get(), px.get()), Q))
@@ -160,33 +59,12 @@ def check_ecSchnorr_sig(
     )
     assert BytesNeq(R, Z20)
     hv = pt.Extract(parity.get(), pt.Int(31), pt.Int(1))
-    eall = Bytes("base16","0xa1dfceb88b3ed8b87f5452a9a2ddbc9e2d3bb9024203785b6ba7b4faea164105")
-    # abi.encodePacked(R, uint8(parity), px, message)
-    tall = Bytes("base16", "0x7c5b88e76cf02299e01e9374ac75c397de3e15d11b88ef3369fc869985783738c1db70ebb0ab6b91a2e7a4fcc2b70ead6e861fcadbd174910b50affeb72000a49a9fafaddd0da395586559227196fcf1743b2c6fc5")
-    rall = pt.Keccak256(Concat(R, hv, px.get(), message.get()))
-    ret = If(pt.BytesEq(eall, rall),  Int(1), Int(0))
-
-
-    #keccak256(abi.encodePacked(R, uint8(parity), px, message))
-    #v = pt.Btoi(pt.Extract(signature, pt.Int(64), pt.Int(1))) - pt.Int(27)
-    # ekecc = pt.Bytes(
-    #             "base16",
-    #             "0x90e504d1a56d29c5e2f371ff81de210956043cb8dbcfc9bee6a09ea975c6e1d5",
-    #         )
-    # eall =   pt.Bytes(
-    #             "base16",
-    #             "0xc02ec0f73e0157b60ecaf687bda9bfb2884a2dfc",
-    #         ) 
-    # rall =  R     
-    # kecc = Keccak256(rall)
-    # ret = If(pt.BytesEq(eall, R),  Int(1), Int(0))
+    #eall = Bytes("base16","0xa1dfceb88b3ed8b87f5452a9a2ddbc9e2d3bb9024203785b6ba7b4faea164105")
+    #tall = Bytes("base16", "0x7c5b88e76cf02299e01e9374ac75c397de3e15d11b88ef3369fc869985783738c1db70ebb0ab6b91a2e7a4fcc2b70ead6e861fcadbd174910b50affeb72000a49a9fafaddd0da395586559227196fcf1743b2c6fc5")
+    rkec256 = pt.Keccak256(Concat(R, hv, px.get(), message.get()))
+    ret = If(pt.BytesEq(e.get(), rkec256),  Int(1), Int(0))
 
     return pt.Seq(
-        # The precompiled lsig should have its address and binary available
-        # here so we can use it to make sure we've been called
-        # with the correct lsig
-        OpUp(mode=pt.OpUpMode.OnCall).maximize_budget(Int(3000)),
-
-        #pt.Assert(BytesEq(eall, rall)),
+        OpUp(mode=pt.OpUpMode.OnCall).ensure_budget(Int(3000),fee_source=pt.OpUpFeeSource.GroupCredit),
         output.set(ret),
     )
--- algorand-bridge/schnorr/EcSchnorrVerifier_test3.py
+++ algorand-bridge/schnorr/EcSchnorrVerifier_test3.py
@@ -23,61 +23,11 @@ def main() -> None:
     app_client.create()
 
 
-    atc = AtomicTransactionComposer()
-
     # Add a payment just to cover fees
     sp_with_fees = algod_client.suggested_params()
     sp_with_fees.flat_fee = True
-    sp_with_fees.fee = beaker.consts.milli_algo * 6
-    atc.add_transaction(
-        TransactionWithSigner(
-            txn=PaymentTxn(acct.address, sp_with_fees, acct.address, 100001),
-            signer=acct.signer,
-        )
-    )
-    atc.add_transaction(
-        TransactionWithSigner(
-            txn=PaymentTxn(acct.address, sp_with_fees, acct.address, 100002),
-            signer=acct.signer,
-        )
-    )
-    atc.add_transaction(
-        TransactionWithSigner(
-            txn=PaymentTxn(acct.address, sp_with_fees, acct.address, 100003),
-            signer=acct.signer,
-        )
-    )
-    atc.add_transaction(
-        TransactionWithSigner(
-            txn=PaymentTxn(acct.address, sp_with_fees, acct.address, 100004),
-            signer=acct.signer,
-        )
-    )
-    atc.add_transaction(
-        TransactionWithSigner(
-            txn=PaymentTxn(acct.address, sp_with_fees, acct.address, 100005),
-            signer=acct.signer,
-        )
-    )
-
-    atc.add_transaction(
-        TransactionWithSigner(
-            txn=PaymentTxn(acct.address, sp_with_fees, acct.address, 100006),
-            signer=acct.signer,
-        )
-    )
+    sp_with_fees.fee = beaker.consts.milli_algo * 5
     
-    sp_no_fee = algod_client.suggested_params()
-    sp_no_fee.flat_fee = True
-
-
-
-    # signature:		0x8260fca590c675be800bbcde4a9ed067ead46612e25b33bc9b6f027ef12326e6 
-    # px:			0x88ef3369fc869985783738c1db70ebb0ab6b91a2e7a4fcc2b70ead6e861fcadb 
-    # py:			0x0000000000000000000000000000000000000000000000000000000000000000 
-    # e:			0xa1dfceb88b3ed8b87f5452a9a2ddbc9e2d3bb9024203785b6ba7b4faea164105 
-    # parity:		0x000000000000000000000000000000000000000000000000000000000000001b 
-    # message:		0xd174910b50affeb72000a49a9fafaddd0da395586559227196fcf1743b2c6fc5
 
     signature = bytes.fromhex("8260fca590c675be800bbcde4a9ed067ead46612e25b33bc9b6f027ef12326e6")
     px      = bytes.fromhex("88ef3369fc869985783738c1db70ebb0ab6b91a2e7a4fcc2b70ead6e861fcadb")
@@ -85,12 +35,9 @@ def main() -> None:
     e       = bytes.fromhex("a1dfceb88b3ed8b87f5452a9a2ddbc9e2d3bb9024203785b6ba7b4faea164105")
     parity  = bytes.fromhex("000000000000000000000000000000000000000000000000000000000000001b")
     message = bytes.fromhex("d174910b50affeb72000a49a9fafaddd0da395586559227196fcf1743b2c6fc5")
+   
 
-    # the middle value:
-
-
-    atc = app_client.add_method_call(
-        atc,
+    tx = app_client.call(
         EcSchnorrVerifier.check_ecSchnorr_sig,
         signature=signature,
         px=px,
@@ -98,62 +45,10 @@ def main() -> None:
         e=e,
         parity=parity,
         message=message,
-        suggested_params=sp_no_fee,
+        suggested_params=sp_with_fees,
     )
+    print('txinfo:', tx.return_value, tx.tx_info)
 
-
-
-    result = atc.execute(algod_client, 3)
-
-    for rv in result.abi_results:
-        print(rv.return_value)        
-
-    # ret = app_client.call(
-    #     EcSchnorrVerifier.check_eth_sig,
-    #     hash=hash,
-    #     signature=signature
-    # )
-    # print("ret:", ret.return_value)
-
-
-    # message = b"OpenZeppelin"
-    # m = keccak.new(digest_bits=256)
-    # m.update(message)
-    # hash = m.digest()
-
-    # sp_no_fee = algod_client.suggested_params()
-    # sp_no_fee.flat_fee = True
-    # # V0
-    # hex_signature = (
-    #     "5d99b6f7f6d1f73d1a26497f2b1c89b24c0993913f86e9a2d02cd69887d9c94f"
-    #     + "3c880358579d811b21dd1b7fd9bb01c1d81d10e69f0384e675c32b39643be8921b"
-    # )
-    # signature = bytes.fromhex(hex_signature)
-    # atc = lsig_client.add_method_call(
-    #     atc,
-    #     eth_checker.check_eth_sig,
-    #     hash=hash,
-    #     signature=signature,
-    #     suggested_params=sp_no_fee,
-    # )
-
-    # # V1
-    # hex_signature = (
-    #     "331fe75a821c982f9127538858900d87d3ec1f9f737338ad67cad133fa48feff"
-    #     + "48e6fa0c18abc62e42820f05943e47af3e9fbe306ce74d64094bdf1691ee53e01c"
-    # )
-    # signature = bytes.fromhex(hex_signature)
-    # atc = lsig_client.add_method_call(
-    #     atc,
-    #     eth_checker.check_eth_sig,
-    #     hash=hash,
-    #     signature=signature,
-    #     suggested_params=sp_no_fee,
-    # )
-
-    # result = atc.execute(algod_client, 4)
-    # for rv in result.abi_results:
-    #     print(rv.return_value)
 
 
 if __name__ == "__main__":
