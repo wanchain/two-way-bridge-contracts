5d3ba17e72c7c4e993ec2dd6b637d94990f3c3c9
lolieatapple <lolieatapple@gmail.com>
2024-01-30 11:17:59 +0800

    Add cancellers and update admin in TimelockController deployment

--- deploy/deploy-evm-timelock.js
+++ deploy/deploy-evm-timelock.js
@@ -3,6 +3,11 @@ const hre = require("hardhat");
 const proposers = ["0x390CC3173EE7F425Fe7659df215B13959FD468E1"];
 const executors = ["0x0000000000000000000000000000000000000000"];
 const admin = '0xa35B3C55626188015aC79F396D0B593947231976';
+const cancellers = [
+  "0x7521eda00e2ce05ac4a9d8353d096ccb970d5188",
+  "0xae693fb903559f8856a3c21d6c0aa4a4e9682ae9",
+  "0xa35b3c55626188015ac79f396d0b593947231976"
+]
 
 // An example of a deploy script that will deploy and call a simple contract.
 async function main() {
@@ -10,9 +15,25 @@ async function main() {
   console.log("Deploying contracts with the account:", deployer);
 
   let TimelockController = await hre.ethers.getContractFactory("TimelockController");
-  let timelockController = await TimelockController.deploy(86400, proposers, executors, admin);
-
+  let timelockController = await TimelockController.deploy(86400, proposers, executors, deployer);
+  await timelockController.deployed();
   console.log("TimelockController deployed to:", timelockController.address);
+  for (let i=0; i<cancellers.length; i++) {
+    console.log('adding canceller', i, cancellers[i]);
+    let tx = await timelockController.grantRole(timelockController.CANCELLER_ROLE(), cancellers[i]);
+    await tx.wait();
+    console.log('added canceller', i, cancellers[i]);
+  }
+
+  console.log('setting admin', admin);
+  let tx = await timelockController.grantRole(timelockController.TIMELOCK_ADMIN_ROLE(), admin);
+  await tx.wait();
+  console.log('set admin ok', admin);
+
+  console.log('revoking deployer admin', deployer);
+  tx = await timelockController.revokeRole(timelockController.TIMELOCK_ADMIN_ROLE(), deployer);
+  await tx.wait();
+  console.log('revoked deployer admin', deployer);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
