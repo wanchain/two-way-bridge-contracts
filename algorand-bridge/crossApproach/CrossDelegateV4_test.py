1ac3feec5ce9e72119ad2b8befde43dd70326f50
Your Name <you@example.com>
2024-02-02 03:47:15 +0000

    upate test script

--- algorand-bridge/crossApproach/CrossDelegateV4_test.py
+++ algorand-bridge/crossApproach/CrossDelegateV4_test.py
@@ -1,9 +1,9 @@
 import os
 from algosdk.abi import ABIType
-from algosdk.atomic_transaction_composer import TransactionWithSigner
+from algosdk.atomic_transaction_composer import TransactionWithSigner, AccountTransactionSigner
 from algosdk.encoding import decode_address, encode_address, encode_as_bytes
 from algosdk.transaction import AssetOptInTxn, PaymentTxn, AssetCreateTxn,AssetTransferTxn
-from algosdk import account, transaction,logic
+from algosdk import account, transaction,logic, util, mnemonic, v2client
 from algosdk.atomic_transaction_composer import (
     AtomicTransactionComposer,
     LogicSigTransactionSigner,
@@ -12,6 +12,7 @@ from algosdk.atomic_transaction_composer import (
 import beaker
 
 import CrossDelegateV4
+IsTestnet = True
 
 
 def print_boxes(app_client: beaker.client.ApplicationClient) -> None:
@@ -23,7 +24,7 @@ def print_boxes(app_client: beaker.client.ApplicationClient) -> None:
             print(bytes.hex(contents))
         else:
             print(f"\t{box_name} => {contents} ")
-def test_userLock(app_client, admin) -> None:
+def test_userLock(app_client, acct_addr, aacct_signer) -> None:
     algod_client = app_client.client
     atc = AtomicTransactionComposer()    
 
@@ -34,8 +35,8 @@ def test_userLock(app_client, admin) -> None:
 
     atc.add_transaction(
         TransactionWithSigner(
-            txn=PaymentTxn(admin.address, sp_with_fees, app_client.app_addr, 300000),
-            signer=admin.signer,
+            txn=PaymentTxn(acct_addr, sp_with_fees, app_client.app_addr, 300000),
+            signer=aacct_signer,
         )
     )
     
@@ -51,7 +52,7 @@ def test_userLock(app_client, admin) -> None:
     result = atc.execute(algod_client, 3)
     for rv in result.abi_results:
         print("---------------------- txUserLock:", rv.return_value)
-def test_userBurn(app_client, admin, assetID) -> None:
+def test_userBurn(app_client, acct_addr, acct_signer,assetID) -> None:
     algod_client = app_client.client
     sp = app_client.get_suggested_params()
     sp.flat_fee = True
@@ -66,12 +67,12 @@ def test_userBurn(app_client, admin, assetID) -> None:
     atc.add_transaction(
         TransactionWithSigner(
             txn=AssetTransferTxn(
-                admin.address,
+                acct_addr,
                 sp,
                 app_client.app_addr,
                 55,assetID,
             ),
-            signer=admin.signer,
+            signer=acct_signer,
         )
     )
 
@@ -81,9 +82,9 @@ def test_userBurn(app_client, admin, assetID) -> None:
         smgID=bytes.fromhex('000000000000000000000000000000000000000000000041726965735f303338'), 
         tokenPairID=33, value=55, fee=55,
         tokenAccount=assetID,
-        userAccount= admin.address,
+        userAccount= acct_addr,
         foreign_assets=[assetID],
-        accounts=[admin.address],
+        accounts=[acct_addr],
     )
 
     result = atc.execute(algod_client, 3)
@@ -91,7 +92,7 @@ def test_userBurn(app_client, admin, assetID) -> None:
         print("---------------------- txUserLock:", rv.return_value)
 
 
-def test_smgMint(app_client, admin, assetID) -> None:
+def test_smgMint(app_client, acct_addr, assetID) -> None:
     sp_big_fee = app_client.get_suggested_params()
     sp_big_fee.flat_fee = True
     sp_big_fee.fee = beaker.consts.milli_algo * 20
@@ -102,11 +103,11 @@ def test_smgMint(app_client, admin, assetID) -> None:
         tokenPairID=33, value=55, 
         fee=1, 
         tokenAccount=assetID,
-        userAccount=admin.address,
+        userAccount=acct_addr,
         r=bytes.fromhex('a423c56d531277a07ae3fb7ef34893c74f5d1f76fa0e1cad047497c413c3fc84000000000000000000000000000000000000000000000000000000000000001c'), 
         s=bytes.fromhex('c23ce3a9f9bf8b4953807fdf3f0fbd7b1b7f8e08f2567515b04ac9687ea66337'),
         foreign_assets=[assetID],
-        accounts=[admin.address],
+        accounts=[acct_addr],
         suggested_params = sp_big_fee,
     )
     print("------------------smgMint:", tx.return_value, tx.tx_info)
@@ -131,7 +132,7 @@ def test_smgRelease(app_client) -> None:
     )
     print("------------------smgRelease:", ttt.return_value, ttt.tx_info)
 
-def test_oracle(app_client, admin) -> None:
+def test_oracle(app_client) -> None:
     sp = app_client.get_suggested_params()
     sp.flat_fee = True
     sp.fee = beaker.consts.milli_algo 
@@ -158,64 +159,78 @@ def test_oracle(app_client, admin) -> None:
     )
     print("------------------get_smg_info:", tx.return_value, tx.tx_info)
 
+class Provider:
+    def __init__(self,isTesn):
+        if(isTesn):
+            mn = "cost piano sample enough south bar diet garden nasty mystery mesh sadness convince bacon best patch surround protect drum actress entire vacuum begin abandon hair"
+            pk = mnemonic.to_private_key(mn)
+            acctAddr = account.address_from_private_key(pk)
+            print(f"testnet Address: {acctAddr}")
+            signer = AccountTransactionSigner(pk)
+            algod_client = v2client.algod.AlgodClient('b7e384d0317b8050ce45900a94a1931e28540e1f69b2d242b424659c341b4697','https://testnet-api.algonode.cloud')
+
+            app_client = beaker.client.ApplicationClient(
+                algod_client, CrossDelegateV4.app, signer=signer
+            )
+            self.acct_signer = signer
+            self.acct_addr = acctAddr
+            self.app_client = app_client
+            self.algod_client = algod_client
+            self.private_key = pk
+        else:
+            accts = beaker.localnet.get_accounts()
+            creator = accts.pop()
+            self.acct_addr = creator.address
+            self.acct_signer = creator.signer
+            self.private_key = creator.private_key
+            app_client = beaker.client.ApplicationClient(
+                beaker.localnet.get_algod_client(), CrossDelegateV4.app, signer=creator.signer
+            )
+            algod_client = app_client.client
+            self.app_client = app_client
+            self.algod_client = algod_client
+            print(f"localnet Address: {creator.address}")
+
+
 
 def main() -> None:
-    accts = beaker.localnet.get_accounts()
-    creator = accts.pop()
-    owner = accts.pop()
-    admin = accts.pop()
+    prov = Provider(IsTestnet)
+    print("prov:", prov.acct_addr)
 
-    app_client = beaker.client.ApplicationClient(
-        beaker.localnet.get_algod_client(), CrossDelegateV4.app, signer=creator.signer
-    )
-    algod_client = app_client.client
+    algod_client = prov.algod_client
+    app_client = prov.app_client
+    acct_addr = prov.acct_addr
+    acct_signer = prov.acct_signer
+    acct_private_key = prov.private_key
 
-    print("creator info:", creator.address, decode_address(creator.address).hex())
-    
     print("Creating app")
     app_client.create()
-    print("Create success")
     print("app_client app_id,app_addr:", app_client.app_id, app_client.app_addr)
 
-    # owner_client = app_client.prepare(signer=owner.signer)
-    # admin_client = app_client.prepare(signer=admin.signer)
-    
-    print("Configing app")
-
-
-    sp_no_fee = app_client.get_suggested_params()
-    sp_no_fee.flat_fee = True
-    sp_no_fee.fee = 0
-
-    
-
     atc = AtomicTransactionComposer()
-
     sp_with_fees = algod_client.suggested_params()
     sp_with_fees.flat_fee = True
     sp_with_fees.fee = beaker.consts.milli_algo
 
     atc.add_transaction(
         TransactionWithSigner(
-            txn=PaymentTxn(creator.address, sp_with_fees, app_client.app_addr, 300001),
-            signer=creator.signer,
+            txn=PaymentTxn(acct_addr, sp_with_fees, app_client.app_addr, 300001),
+            signer=acct_signer,
         )
     )
-    
 
     atc = app_client.add_method_call(
         atc,
         CrossDelegateV4.initialize,
-        owner=creator.address,
-        admin=creator.address,
+        owner=acct_addr,
+        admin=acct_addr,
         oracle_id=1,
         token_manager_id=2,
     )
-
     result = atc.execute(algod_client, 3)
 
     ################ oracle #######################
-    test_oracle(app_client, admin)
+    test_oracle(app_client)
 
     
 
@@ -227,7 +242,7 @@ def main() -> None:
     #     boxes=[(app_client.app_id, bytes.fromhex('029a'))]
     # )
 
-    # balance_before_get = app_client.client.account_info(creator.address)
+    # balance_before_get = app_client.client.account_info(acct_addr)
     # print("balance_before_get:", balance_before_get['amount'])
     # fee = app_client.call(
     #     CrossDelegateV4.getTokenPairFee,
@@ -236,7 +251,7 @@ def main() -> None:
     #     suggested_params=sp
     # )
     # print("fee:", fee.return_value)
-    # balance_after_get = app_client.client.account_info(creator.address)
+    # balance_after_get = app_client.client.account_info(acct_addr)
     # print("balance_after_get:", balance_after_get['amount'])
     
 
@@ -269,7 +284,7 @@ def main() -> None:
     # print("testGetInfo:", testGetInfo.return_value)
 
     #userLock
-    test_userLock(app_client, admin)
+    test_userLock(app_client, acct_addr, acct_signer)
 
     
 
@@ -295,9 +310,9 @@ def main() -> None:
     sp.flat_fee = True
     sp.fee = 2000
     optin_txn = transaction.AssetOptInTxn(
-        sender=admin.address, sp=sp, index=assetID
+        sender=acct_addr, sp=sp, index=assetID
     )
-    signed_optin_txn = optin_txn.sign(admin.private_key)
+    signed_optin_txn = optin_txn.sign(acct_private_key)
 
     txid = app_client.client.send_transaction(signed_optin_txn)
     print(f"Sent opt in transaction with txid: {txid}")
@@ -306,10 +321,10 @@ def main() -> None:
     results = transaction.wait_for_confirmation(app_client.client, txid, 4)
 
 
-    test_smgMint(app_client, admin, assetID) 
+    test_smgMint(app_client, acct_addr, assetID) 
 
     
-    test_userBurn(app_client, admin, assetID)
+    test_userBurn(app_client, acct_addr, acct_signer, assetID)
 
 
 
