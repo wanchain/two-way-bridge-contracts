3fe6c0d7c675728112efa3282290f2dceea39089
Your Name <you@example.com>
2024-04-25 10:12:16 +0000

    delete useless files

--- algorand-bridge/crossApproach/lib/EtherTransfer.py
+++
@@ -1,9 +0,0 @@
-from pyteal import *
-from beaker import *
-from beaker.lib.storage import BoxMapping
-from enum import Enum
-
-class EtherTransfer:
-  # sendValue(address payable recipient, uint256 amount, uint256 gasLimit)
-  def sendValue(recipient: abi.Address, amount: abi.Uint64, gasLimit: abi.Uint64):
-    assert()
