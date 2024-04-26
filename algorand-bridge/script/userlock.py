cc6dd16373627447c221ea5d771bc987010d5167
Richard <Richard@wanchain.org>
2024-04-26 11:28:37 +0800

    update test script

--- algorand-bridge/script/userlock.py
+++ algorand-bridge/script/userlock.py
@@ -123,7 +123,7 @@ def test_smgRelease(app_client) -> None:
     sp_big_fee.flat_fee = True
     sp_big_fee.fee = beaker.consts.milli_algo * 20
     uniqueID=bytes.fromhex('3260fca590c675be800bbcde4a9ed067ead46612e25b33bc9b6f027ef12326e6')
-    smgID = bytes.fromhex('000000000000000000000000000000000000000000746573746e65745f303632')
+    smgID=bytes.fromhex('000000000000000000000000000000000000000000746573746e65745f303631')
 
     ttt = app_client.call(
         bridge.smgRelease,
@@ -131,8 +131,8 @@ def test_smgRelease(app_client) -> None:
         smgID=smgID, 
         tokenPairID=33, value=55, 
         fee=1, tokenAccount=0,
-        r=bytes.fromhex('96d21ff6c85c8fe2b35263cd87e7e8cfc2fea1e15e9169815c42c10510a10232000000000000000000000000000000000000000000000000000000000000001c'), 
-        s=bytes.fromhex('6e90148c7c9fda60edaeb2bfb4de6334220961307808933d76e1eab147f83f28'),
+        r=bytes.fromhex('a423c56d531277a07ae3fb7ef34893c74f5d1f76fa0e1cad047497c413c3fc84000000000000000000000000000000000000000000000000000000000000001c'), 
+        s=bytes.fromhex('c23ce3a9f9bf8b4953807fdf3f0fbd7b1b7f8e08f2567515b04ac9687ea66337'),
         userAccount="7LTVKXWHLGFI4FP6YCACSS4DPSZ6IQBHJXRYX53QVQRXDTGIK6KSU4J7ZY",
         suggested_params = sp_big_fee,
         boxes=[(app_client.app_id, smgID), (app_client.app_id, uniqueID)], # Must append app_id and box key for tx
@@ -273,22 +273,11 @@ def main() -> None:
     # updateTokenPair(app_client, AssetID)
     test_userLock(app_client, prov.acct_addr, prov.acct_signer)
     test_userLockToken(app_client, prov.acct_addr, prov.acct_signer)
-    return
-
     
     test_smgRelease(app_client)
     return
 
 
-
-
-    test_smgMint(app_client, acct_addr, assetID) 
-    test_userBurn(app_client, acct_addr, acct_signer, assetID)
-
-
-
-    print_boxes(app_client)
-    return
 
 
 if __name__ == "__main__":
