acfc3dadad972ec9265d00baa9343e275cb725d5
Jacob <36022598+13552128609@users.noreply.github.com>
2024-08-28 16:19:35 +0800

    change admin of cross ok

---
+++ ton-bridge/tests/Bridge.spec.ts
@@ -0,0 +1,102 @@
+import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
+import {Address, Cell, toNano} from '@ton/core';
+import { Bridge } from '../wrappers/Bridge';
+import '@ton/test-utils';
+import { compile } from '@ton/blueprint';
+
+describe('Bridge', () => {
+    let code: Cell;
+
+    beforeAll(async () => {
+        code = await compile('Bridge');
+    });
+
+    let blockchain: Blockchain;
+    let deployer: SandboxContract<TreasuryContract>;
+    let bridge: SandboxContract<Bridge>;
+
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+        deployer = await blockchain.treasury('deployer');
+
+        let c = Bridge.createFromConfig(
+            {
+                owner: deployer.address,
+                admin:deployer.address,
+                halt:0,
+                init:0,
+            },
+            code
+        )
+
+        bridge = blockchain.openContract(c);
+
+        //deployer = await blockchain.treasury('deployer');
+
+
+        const deployResult = await bridge.sendDeploy(deployer.getSender(), toNano('1000'));
+
+        //console.log("deployResult==>",deployResult.transactions);
+
+        console.log("deployer.address==>",deployer.address);
+        console.log("deployer.address(bigInt)==>",BigInt("0x"+deployer.address.hash.toString('hex')));
+        console.log("bridge.address==>",bridge.address);
+        console.log("bridge.address(bigInt)==>",BigInt("0x"+bridge.address.hash.toString('hex')));
+
+
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: bridge.address,
+            deploy: true,
+            success: true,
+        });
+    });
+
+
+
+    it('should deploy', async () => {
+        // the check is done inside beforeEach
+        // blockchain and counter are ready to use
+    });
+
+    it('should addAdmin success', async () => {
+
+        let retCrossConfigOld = await bridge.getCrossConfig()
+        console.log("retCrossConfigOld",retCrossConfigOld);
+
+        let oldAdmin = retCrossConfigOld.admin;
+        console.log("oldAdmin",oldAdmin);
+
+        const user1 = await blockchain.treasury('user1');
+        const queryID=1;
+
+        console.log("user1.address==>",user1.address);
+        console.log("user1.address(bigInt)==>",BigInt("0x"+user1.address.hash.toString('hex')));
+
+        console.log("adminAddr",deployer.address.toRawString());
+        const ret = await bridge.sendAddAdmin(deployer.getSender(), {
+            adminAddr:user1.address,
+            value: toNano('1000'),
+            queryID,
+        });
+
+        console.log("ret",ret);
+
+        let retCrossConfig = await bridge.getCrossConfig()
+        console.log("retCrossConfig",retCrossConfig);
+
+        let newAdmin = retCrossConfig.admin;
+        console.log("newAdmin",newAdmin);
+
+        expect(ret.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: bridge.address,
+            success: true,
+        });
+
+        expect(newAdmin.equals(user1.address));
+
+    });
+    
+
+});
---
+++ ton-bridge/wrappers/Bridge.ts
@@ -0,0 +1,128 @@
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
+
+export type BridgeConfig = {
+    owner:Address,
+    admin:Address,
+    halt: number,
+    init: number,
+};
+
+export type CrossConfig = {
+    owner:Address,
+    admin:Address,
+    halt: number,
+    init: number,
+};
+
+export function bridgeConfigToCell(config: BridgeConfig): Cell {
+    return beginCell()
+        .storeAddress(config.owner)
+        .storeAddress(config.admin)
+        .storeUint(config.halt,2)
+        .storeUint(config.init,2)
+        .storeRef(beginCell().endCell())
+        .storeRef(beginCell().endCell())
+        .storeRef(beginCell().endCell())
+        .storeRef(beginCell().endCell())
+        .endCell();
+}
+
+export const Opcodes = {    
+    verify: 0x7e8764ef,
+    verifyEcdsa: 0x7e8764ee,
+
+    addAdmin: 0x01,
+    removeAdmin: 0x02,
+    transferOwner: 0x03,
+    setHalt: 0x04,
+    initialize: 0x05,
+
+
+    setFee: 0x10,
+    setFees: 0x11,
+
+    userLock: 0x12,
+    smgMint: 0x13,
+
+    userBurn: 0x14,
+    userRelease: 0x15,
+
+    setSmgFeeProxy: 0x16,
+
+    setTokenPairFee: 0x17,
+    setTokenPairFees: 0x18,
+
+// token manager
+    addTokenPair: 0x21,
+    removeTokenPair: 0x22,
+    updateTokenPair: 0x23,
+
+// op about oracle
+    transferOracleAdmin: 0x31,
+    setStoremanGroupConfig: 0x32,
+    setStoremanGroupStatus: 0x33,
+    acquireReadySmgInfo: 0x35,
+
+// op about groupApprove
+    transferFoundation: 0x41,
+    AcquireReadySmgInfo: 0x42,
+    proposal: 0x43,
+    approveAndExecute: 0x44,
+    Initialize: 0x45,
+};
+
+export class Bridge implements Contract {
+    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+
+    static createFromAddress(address: Address) {
+        return new Bridge(address);
+    }
+
+    static createFromConfig(config: BridgeConfig, code: Cell, workchain = 0) {
+        const data = bridgeConfigToCell(config);
+        const init = { code, data };
+        return new Bridge(contractAddress(workchain, init), init);
+    }
+
+    async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().endCell(),
+        });
+    }
+
+    async sendAddAdmin(
+        provider: ContractProvider,
+        via: Sender,
+        opts: {
+            adminAddr:Address,
+            value: bigint;
+            queryID?: number;
+        }
+    ) {
+        let isValid = Address.isAddress(opts.adminAddr)
+        if (!isValid){
+            await Promise.reject("in valid address")
+        }
+        await provider.internal(via, {
+            value: opts.value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(Opcodes.addAdmin, 32)
+                .storeUint(opts.queryID ?? 0, 64)
+                .storeAddress(opts.adminAddr)
+                .endCell(),
+        });
+    }
+
+    async getCrossConfig(provider: ContractProvider) {
+        const result = await provider.get('get_cross_config', []);
+        return {
+            owner: result.stack.readAddress(),
+            admin: result.stack.readAddress(),
+            halt:result.stack.readNumber(),
+            init:result.stack.readNumber()
+        }
+    }
+}
