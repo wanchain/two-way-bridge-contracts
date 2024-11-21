0d5f33ec196d58ff7b920fe7c4f3f2cf461607b0
lolieatapple <lolieatapple@gmail.com>
2024-11-21 15:10:02 +0800

    update cross wrapper add transfer and approve

--- contracts/components/CrossWrapper.sol
+++ contracts/components/CrossWrapper.sol
@@ -27,25 +27,53 @@
 //
 
 pragma solidity 0.8.18;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 
 interface ICross {
+    function currentChainID() external view returns (uint);
+    function getPartners() external view returns(address tokenManager, address smgAdminProxy, address smgFeeProxy, address quota, address sigVerifier);
     function userLock(bytes32 smgID, uint tokenPairID, uint value, bytes calldata userAccount) external payable;
     function userBurn(bytes32 smgID, uint tokenPairID, uint value, uint fee, address tokenAccount, bytes calldata userAccount) external payable;
     function userLockNFT(bytes32 smgID, uint tokenPairID, uint[] memory tokenIDs, uint[] memory tokenValues, bytes memory userAccount) external payable;
     function userBurnNFT(bytes32 smgID, uint tokenPairID, uint[] memory tokenIDs, uint[] memory tokenValues, address tokenAccount, bytes memory userAccount) external payable;
 }
 
-contract CrossWrapper {
+interface ITokenManager {
+    function getTokenPairInfo(uint id) external view returns (uint fromChainID, bytes memory fromAccount, uint toChainID, bytes memory toAccount);
+    function mapTokenPairType(uint id) external view returns (uint8);
+}
+
+contract CrossWrapper is ERC721Holder, ERC1155Holder {
+    using SafeERC20 for IERC20;
+
     ICross public cross;
+    address public tokenManager;
+    uint public currentChainID;
+
+    enum TokenCrossType {ERC20, ERC721, ERC1155}
 
     event PartnerCross(string partner);
 
     constructor(address _cross) {
         cross = ICross(_cross);
+        (tokenManager, , , , ) = cross.getPartners();
+        currentChainID = cross.currentChainID();
     }
 
     function userLock(bytes32 smgID, uint tokenPairID, uint value, bytes calldata userAccount, string memory partner) external payable {
-        cross.userLock{value: msg.value}(smgID, tokenPairID, value, userAccount);
+        address tokenAddress = _getTokenAddressFromPairID(tokenPairID);
+        if (tokenAddress == address(0)) {
+            cross.userLock{value: msg.value}(smgID, tokenPairID, value, userAccount);
+        } else {
+            IERC20(tokenAddress).safeTransferFrom(msg.sender, address(this), value);
+            IERC20(tokenAddress).approve(address(cross), value);
+            cross.userLock{value: msg.value}(smgID, tokenPairID, value, userAccount);
+        }
         emit PartnerCross(partner);
     }
 
@@ -55,6 +83,19 @@ contract CrossWrapper {
     }
 
     function userLockNFT(bytes32 smgID, uint tokenPairID, uint[] memory tokenIDs, uint[] memory tokenValues, bytes memory userAccount, string memory partner) external payable {
+        uint8 tokenCrossType = ITokenManager(tokenManager).mapTokenPairType(tokenPairID);
+        address tokenScAddr = _getTokenAddressFromPairID(tokenPairID);
+        if (tokenCrossType == uint8(TokenCrossType.ERC721)) {
+            for(uint idx = 0; idx < tokenIDs.length; ++idx) {
+                IERC721(tokenScAddr).safeTransferFrom(msg.sender, address(this), tokenIDs[idx], "");
+            }
+            IERC721(tokenScAddr).setApprovalForAll(address(cross), true);
+        } else if(tokenCrossType == uint8(TokenCrossType.ERC1155)) {
+            IERC1155(tokenScAddr).safeBatchTransferFrom(msg.sender, address(this), tokenIDs, tokenValues, "");
+            IERC1155(tokenScAddr).setApprovalForAll(address(cross), true);
+        } else {
+            require(false, "Invalid NFT type");
+        }
         cross.userLockNFT{value: msg.value}(smgID, tokenPairID, tokenIDs, tokenValues, userAccount);
         emit PartnerCross(partner);
     }
@@ -62,5 +103,22 @@ contract CrossWrapper {
     function userBurnNFT(bytes32 smgID, uint tokenPairID, uint[] memory tokenIDs, uint[] memory tokenValues, address tokenAccount, bytes memory userAccount, string memory partner) external payable {
         cross.userBurnNFT{value: msg.value}(smgID, tokenPairID, tokenIDs, tokenValues, tokenAccount, userAccount);
         emit PartnerCross(partner);
+    }
+
+    function _getTokenAddressFromPairID(uint tokenPairID) internal view returns (address) {
+        (uint fromChainID, bytes memory fromAccount, uint toChainID, bytes memory toAccount) = ITokenManager(tokenManager).getTokenPairInfo(tokenPairID);
+        if (currentChainID == fromChainID) {
+            return _bytesToAddress(fromAccount);
+        } else if (currentChainID == toChainID) {
+            return _bytesToAddress(toAccount);
+        } else {
+            revert("Invalid token pair ID");
+        }
+    }
+
+    function _bytesToAddress(bytes memory b) internal pure returns (address addr) {
+        assembly {
+            addr := mload(add(b,20))
+        }
     }
 }
