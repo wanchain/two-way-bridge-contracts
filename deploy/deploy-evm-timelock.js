34e899851a05c0ed665d31469da4caa9da413745
linaly <gao.gaoshuang@gmail.com>
2025-04-25 10:16:05 +0000

    update deploy-evm-timelock

--- deploy/deploy-evm-timelock.js
+++ deploy/deploy-evm-timelock.js
@@ -2,22 +2,23 @@ const hre = require("hardhat");
 
 const proposers = ["0x390CC3173EE7F425Fe7659df215B13959FD468E1"];
 const executors = ["0x0000000000000000000000000000000000000000"];
-const admin = '0xa35B3C55626188015aC79F396D0B593947231976';
+const admin = '0x6f0889f42BaA6a0F815cE3E93530bcf2d6a711bE';
 const cancellers = [
-  "0x7521eda00e2ce05ac4a9d8353d096ccb970d5188",
-  "0xae693fb903559f8856a3c21d6c0aa4a4e9682ae9",
-  "0xa35b3c55626188015ac79f396d0b593947231976"
+  "0xf0619c7e4d442961731c0721628b8156294cd578",
+  "0xa32220d02acff019e3c4075b4c086b5f4cdff5be",
+  "0x6f0889f42BaA6a0F815cE3E93530bcf2d6a711bE"
 ]
 
 // An example of a deploy script that will deploy and call a simple contract.
 async function main() {
-  let deployer = (await hre.ethers.getSigner()).address;
+  let deployer = (await hre.ethers.getSigners())[0].address;
   console.log("Deploying contracts with the account:", deployer);
 
   let TimelockController = await hre.ethers.getContractFactory("TimelockController");
   let timelockController = await TimelockController.deploy(86400, proposers, executors, deployer);
-  await timelockController.deployed();
-  console.log("TimelockController deployed to:", timelockController.address);
+  await (timelockController.deployed ?  timelockController.deployed() : timelockController.waitForDeployment());
+
+  console.log("TimelockController deployed to:", timelockController.target);
   for (let i=0; i<cancellers.length; i++) {
     console.log('adding canceller', i, cancellers[i]);
     let tx = await timelockController.grantRole(timelockController.CANCELLER_ROLE(), cancellers[i]);
