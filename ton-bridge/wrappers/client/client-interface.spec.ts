57b103fa90af7c2d94d3851af25d0334b9e8fe96
Jacob <36022598+13552128609@users.noreply.github.com>
2025-05-13 17:50:00 +0800

    update WanTonClient interface standard

--- ton-bridge/wrappers/client/client-interface.spec.ts
+++ ton-bridge/wrappers/client/client-interface.spec.ts
@@ -1,26 +1,36 @@
 
-import {getClient, TonClientConfig} from "./client";
-import {WanTonClient} from "./client-interface";
+import {getClient, TonClientConfig, wanTonSdkInit} from "./client";
+import {IsWanTonClient, WanTonClient} from "./client-interface";
+import {Blockchain} from "@ton/sandbox";
+import {configTestnet, configTestTonApi} from "../config/config-ex";
 
 let client:WanTonClient;
-
-// function isInterface(obj:any,inter:wanTonClient){
-//     return true;
-// }
 
 describe('client interface', () => {
 
     beforeAll(async () => {
+
+    },50000);
+
+    it('client tonApi', async () => {
+        let config = Object.assign({},configTestTonApi);
+        config.usingDbCache = false;
+        await wanTonSdkInit(config);
         client = await getClient();
-        // if(client instanceof wanTonClient){
-        //     console.log("interface ok");
-        // }else{
-        //     console.log("interface KO");
-        // }
+        if(IsWanTonClient(client)){
+            console.log("interface ok");
+        }else{
+            console.log("interface KO");
+        }
     },50000);
 
     it('client interface', async () => {
-
+        let blockchain = await Blockchain.create();
+        if(IsWanTonClient(blockchain)){
+            console.log("interface ok");
+        }else{
+            console.log("interface KO");
+        }
     },500000);
 
 });
