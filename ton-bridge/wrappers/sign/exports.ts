2b37ed4fb0ff203e04667cb3377788e29e943e08
Jacob <36022598+13552128609@users.noreply.github.com>
2025-06-25 18:48:25 +0800

    update

--- ton-bridge/wrappers/sign/exports.ts
+++ ton-bridge/wrappers/sign/exports.ts
@@ -1,3 +1,3 @@
 export * from './buildHash'
 export * from './rawTrans'
-export const toolsSec256k1 = require('./tools-secp256k1')
+//export const toolsSec256k1 = require('./tools-secp256k1')  // only for internal use.
