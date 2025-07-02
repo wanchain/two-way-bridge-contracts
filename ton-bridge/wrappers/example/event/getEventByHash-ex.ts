38bd24068dc88654821ecdfe9f747325dab9282f
Jacob <36022598+13552128609@users.noreply.github.com>
2025-07-02 15:57:54 +0800

    updage getEventByHash

--- ton-bridge/wrappers/example/event/getEventByHash-ex.ts
+++ ton-bridge/wrappers/example/event/getEventByHash-ex.ts
@@ -1,42 +1,70 @@
-import {sleep} from "../../utils/utils";
-
-import {configTestnet, configMainnet, configTestTonApi} from "../../config/config-ex";
-import {getClient, TonClientConfig, wanTonSdkInit} from "../../client/client";
-import {getEventByTranHash, getEvents} from "../../event/getEvents";
-import { logger } from "../../utils/logger";
+import {configMainnet, configTestTonApi} from "../../config/config-ex";
+import {getClient, wanTonSdkInit} from "../../client/client";
+import {getEventByTranHash} from "../../event/getEvents";
 import {IsWanTonClient} from "../../client/client-interface";
 
-const args = process.argv.slice(2);
+const optimist = require('optimist');
+let argv = optimist
+    .usage("Usage: $0")
+    .alias('h', 'help')
+    .describe('network', 'network name testnet|mainnet')
+    .describe('contractAddr', 'contractAddr')
+    .describe('lt', 'logical time')
+    .describe('hash', 'transaction hash')
+    .argv;
 
-async function main(){
-    //await wanTonSdkInit(configMainnet);
-    //await wanTonSdkInit(configTestnet);
-    await wanTonSdkInit(configTestTonApi)
-    try{
-        let client = await getClient();
-        let scBridgeAddr = args[0];
-        let lt = args[1];
-        let tranHash = Buffer.from(args[2],'hex').toString('base64');
+console.log(optimist.argv);
+console.log((Object.getOwnPropertyNames(optimist.argv)).length);
 
-        console.log("scBridgeAddr",scBridgeAddr,"lt",lt,"tranHash",tranHash);
-        console.log("before getEventByTranHash","client is WanTonClient",IsWanTonClient(client));
-        let ret = await getEventByTranHash(client,scBridgeAddr,lt,tranHash);
 
-        console.log("ret = ",ret);
-        client = null;
-    }catch(err){
-        console.error(err.code,err.response?.data?.error)
+if ((Object.getOwnPropertyNames(optimist.argv)).length < 4) {
+    optimist.showHelp();
+    process.exit(0);
+}
+
+
+global.network = argv["network"];
+const config = require('../../config/config');
+
+let client = null;
+
+async function init() {
+    if (global.network == 'testnet') {
+        await wanTonSdkInit(configTestTonApi);
+    } else {
+        await wanTonSdkInit(configMainnet);
     }
+
+    client = await getClient();
+}
+
+async function getEventByHash() {
+    try {
+        let scBridgeAddr = argv['contractAddr'];
+        let lt = argv['lt'];
+        let tranHash = Buffer.from(argv['hash'], 'hex').toString('base64');
+
+        console.log("scBridgeAddr", scBridgeAddr, "lt", lt, "tranHash", tranHash);
+        console.log("before getEventByTranHash", "client is WanTonClient", IsWanTonClient(client));
+        let ret = await getEventByTranHash(client, scBridgeAddr, lt, tranHash);
+
+        console.log("ret = ", ret);
+        client = null;
+    } catch (err) {
+        console.error(err.code, err.response?.data?.error)
+    }
+}
+
+async function main() {
+    await init();
+    await getEventByHash();
 
 }
 
 main();
 // userLock
-// ts-node getEventByHash-ex.ts kQDlYDH0PmST2okwTluXJ2mUDMDCzPzXF1gGz24U6H2tE9Wr 33313072000003 5c6cbcef28ef2514ea0b7594a0dda35e4ce08416312d3c5629388ae21ad54e83
-// ts-node getEventByHash-ex.ts kQDlYDH0PmST2okwTluXJ2mUDMDCzPzXF1gGz24U6H2tE9Wr 33313091000003 095015c23c8323af4b7c20b9bd35b2864aa2a4fbaa85335828fa63bccbfdaeff
-// ts-node getEventByHash-ex.ts kQDlYDH0PmST2okwTluXJ2mUDMDCzPzXF1gGz24U6H2tE9Wr 33313098000001 391972a7343d4b644e279575ae1e7b4c92fcf4deda69f3fbe5cb5b7b9ea67f2a
+// ts-node getEventByHash-ex.ts --network testnet --contractAddr kQDlYDH0PmST2okwTluXJ2mUDMDCzPzXF1gGz24U6H2tE9Wr --lt 33313072000003 --hash 5c6cbcef28ef2514ea0b7594a0dda35e4ce08416312d3c5629388ae21ad54e83
+
 
 // smgRelease
-// ts-node getEventByHash-ex.ts kQDlYDH0PmST2okwTluXJ2mUDMDCzPzXF1gGz24U6H2tE9Wr 33315421000001 5e654b1798cca9b63adcb8091b0962b364581538bc4431449f9e9c674bb0d5f6
-// ts-node getEventByHash-ex.ts kQDlYDH0PmST2okwTluXJ2mUDMDCzPzXF1gGz24U6H2tE9Wr 33315410000001 dd22c46a31a348d5ff0b77bb0191b915549af7c450ab905390b3e7d8e6981c32
-// ts-node getEventByHash-ex.ts kQDlYDH0PmST2okwTluXJ2mUDMDCzPzXF1gGz24U6H2tE9Wr 33315399000001 458f7592928e02d22584a71b290abbf82def08c618ee26acbf2da3ea2eccb7a2
+// ts-node getEventByHash-ex.ts --network testnet --contractAddr kQDlYDH0PmST2okwTluXJ2mUDMDCzPzXF1gGz24U6H2tE9Wr --lt 33315421000001 --hash 5e654b1798cca9b63adcb8091b0962b364581538bc4431449f9e9c674bb0d5f6
