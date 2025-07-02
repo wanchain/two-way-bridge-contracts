db4271a9568f939256d4158e63626065ff4058cf
Jacob <36022598+13552128609@users.noreply.github.com>
2025-07-02 16:16:24 +0800

    update getEventsByRange for mainnet

--- ton-bridge/wrappers/example/event/getEventsByRange-ex.ts
+++ ton-bridge/wrappers/example/event/getEventsByRange-ex.ts
@@ -1,22 +1,50 @@
-import {bigIntReplacer, sleep} from "../../utils/utils";
-import {configTestnet, configMainnet, configTestTonApi, configTestTonApiNoDb} from "../../config/config-ex";
-import {getClient, TonClientConfig, wanTonSdkInit} from "../../client/client";
-import {getEventByTranHash, getEvents} from "../../event/getEvents";
+import {sleep} from "../../utils/utils";
+import {configMainnet, configTestTonApi} from "../../config/config-ex";
+import {getClient, wanTonSdkInit} from "../../client/client";
+import {getEventByTranHash} from "../../event/getEvents";
 import {DBAccess} from "../../db/DbAccess";
 import {Address} from "@ton/core";
 import {convertTranToTonTrans} from "../../db/common";
 
-const args = process.argv.slice(2);
+const optimist = require('optimist');
+let argv = optimist
+    .usage("Usage: $0")
+    .alias('h', 'help')
+    .describe('network', 'network name testnet|mainnet')
+    .describe('contractAddr', 'contractAddr')
+    .describe('lt', 'logical time')
+    .describe('to_lt', 'to logical time')
+    .argv;
 
-async function main() {
-    //await wanTonSdkInit(configMainnet);
-    //await wanTonSdkInit(configTestnet);
-    await wanTonSdkInit(configTestTonApi);
-    //await wanTonSdkInit(configTestTonApiNoDb);
+console.log(optimist.argv);
+console.log((Object.getOwnPropertyNames(optimist.argv)).length);
 
-    let scBridgeAddr = args[0];
-    let lt = BigInt(args[1]);
-    let to_lt = BigInt(args[2]);
+
+if ((Object.getOwnPropertyNames(optimist.argv)).length < 4) {
+    optimist.showHelp();
+    process.exit(0);
+}
+
+
+global.network = argv["network"];
+const config = require('../../config/config');
+
+let client = null;
+
+async function init() {
+    if (global.network == 'testnet') {
+        await wanTonSdkInit(configTestTonApi);
+    } else {
+        await wanTonSdkInit(configMainnet);
+    }
+
+    client = await getClient();
+}
+
+async function getEventByRange() {
+    let scBridgeAddr = argv['contractAddr'];
+    let lt = BigInt(argv['lt']);
+    let to_lt = BigInt(argv['to_lt']);
     let dbAcces = await DBAccess.getDBAccess();
     if (!dbAcces) {
         console.error("not using db cache");
@@ -40,7 +68,7 @@ async function main() {
             await sleep(2000);
         }
         try {
-            tonTrans = await dbAcces.getAllTransNotHandledByRange(args[0],lt, to_lt);
+            tonTrans = await dbAcces.getAllTransNotHandledByRange(scBridgeAddr, lt, to_lt);
             console.log("getAllTransNotHandledByRange tonTrans.length", tonTrans.length);
         } catch (err) {
             console.error(err.code, err.response?.data?.error)
@@ -52,7 +80,7 @@ async function main() {
                 let ret = await getEventByTranHash(client, scBridgeAddr, tonTran.lt.toString(10), tonTran.hash().toString('hex'));
                 console.log("JacobEvent ret = ", ret);
                 let tranTonTemp = convertTranToTonTrans([tonTran]);
-                await dbAcces.setTranHandleFlag(scBridgeAddr,tranTonTemp[0],true);
+                await dbAcces.setTranHandleFlag(scBridgeAddr, tranTonTemp[0], true);
             } catch (err) {
                 console.error(err.code, err.response?.data?.error)
             }
@@ -60,9 +88,13 @@ async function main() {
         }
 
     }, 100000)
+}
 
+async function main() {
+    await init();
+    await getEventByRange();
 }
 
 main();
 
-// ts-node getAllEvents-ex.ts kQDlYDH0PmST2okwTluXJ2mUDMDCzPzXF1gGz24U6H2tE9Wr 35100832000001 33781871000003
+// ts-node getAllEvents-ex.ts --network testnet --contractAddr kQDlYDH0PmST2okwTluXJ2mUDMDCzPzXF1gGz24U6H2tE9Wr --lt 35100832000001 --to_lt 33781871000003
