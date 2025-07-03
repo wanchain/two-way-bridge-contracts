4d9a79cd87b798cffa9bfcb4de5a0780685919b1
Jacob <36022598+13552128609@users.noreply.github.com>
2025-07-03 17:00:40 +0800

    update isTranSuccess-ex.ts

--- ton-bridge/wrappers/example/tran/isTranSuccess-ex.ts
+++ ton-bridge/wrappers/example/tran/isTranSuccess-ex.ts
@@ -1,33 +1,62 @@
-import {sleep} from "../../utils/utils";
+import {toBase64} from "../../utils/utils";
 
-import {configTestnet,configMainnet} from "../../config/config-ex";
-import {getClient, TonClientConfig, wanTonSdkInit} from "../../client/client";
-import {getEventByTranHash, getEvents, getTransaction} from "../../event/getEvents";
-import { logger } from "../../utils/logger";
+import {configMainnetNoDb, configTestTonApiNoDb} from "../../config/config-ex";
+import {getClient, wanTonSdkInit} from "../../client/client";
+import {getTransaction} from "../../event/getEvents";
 import {isTranSuccess} from "../../transResult/transResult";
 
-const args = process.argv.slice(2);
+const optimist = require('optimist');
+let argv = optimist
+    .usage("Usage: $0")
+    .alias('h', 'help')
+    .describe('network', 'network name testnet|mainnet')
+    .describe('contractAdd', 'contract address')
+    .describe('lt', 'logic time')
+    .describe('txHash', 'tx hash base64|hexstring')
+    .argv;
 
-async function main(){
-    try{
-        await wanTonSdkInit(configMainnet);
-        await wanTonSdkInit(configTestnet);
-        let client = await getClient();
-        let scBridgeAddr = args[0];
-        let lt = args[1];
-        let tranHash = Buffer.from(args[2],'hex').toString('base64');
+console.log(optimist.argv);
+console.log((Object.getOwnPropertyNames(optimist.argv)).length);
 
-        console.log("scBridgeAddr",scBridgeAddr,"lt",lt,"tranHash",tranHash);
-        let tran = await getTransaction(client,scBridgeAddr,lt,tranHash);
+
+if ((Object.getOwnPropertyNames(optimist.argv)).length < 4) {
+    optimist.showHelp();
+    process.exit(0);
+}
+
+global.network = argv["network"];
+const config = require('../../config/config');
+
+let client = null;
+
+async function init() {
+    if (global.network == 'testnet') {
+        await wanTonSdkInit(configTestTonApiNoDb);
+    } else {
+        await wanTonSdkInit(configMainnetNoDb);
+    }
+
+    client = await getClient();
+}
+
+async function main() {
+    try {
+        await init();
+        let scBridgeAddr = argv['contractAddr'];
+        let lt = argv['lt'];
+        let tranHash = toBase64(argv['txHash'])
+
+        console.log("scBridgeAddr", scBridgeAddr, "lt", lt, "tranHash", tranHash);
+        let tran = await getTransaction(client, scBridgeAddr, lt, tranHash);
         let ret = await isTranSuccess(tran);
-        console.log("ret = ",ret);
+        console.log("ret = ", ret);
         client = null;
-    }catch(err){
-        console.error(err.code,err.response?.data?.error)
+    } catch (err) {
+        console.error(err.code, err.response?.data?.error)
     }
 
 }
 
 main();
 
-// ts-node isTranSuccess-ex.ts EQCABVjsQnmRELMK6vjwGbYNRzHXoTd2hvSX6v_VmVrrJNjW 33028013000001 f0f311fecb1775047c741a25b6b714026cd4336d987a956fa7363ad7bec7cab4
+// ts-node isTranSuccess-ex.ts --network testnet --contractAddr EQCABVjsQnmRELMK6vjwGbYNRzHXoTd2hvSX6v_VmVrrJNjW --lt 33028013000001 --txHash f0f311fecb1775047c741a25b6b714026cd4336d987a956fa7363ad7bec7cab4
