76d2c620edddf6fadc399f2c1b2bad56a7dfa53a
Jacob <36022598+13552128609@users.noreply.github.com>
2025-07-03 10:28:48 +0800

    check input parameter in buildSmgReleasemessage

--- ton-bridge/wrappers/code/smgRelease.ts
+++ ton-bridge/wrappers/code/smgRelease.ts
@@ -2,7 +2,7 @@ import {Address} from "@ton/core";
 import * as opcodes from "../opcodes";
 import {logger} from '../utils/logger';
 import {codeTable} from "./encode-decode";
-import {bigIntReplacer, getQueryID} from "../utils/utils";
+import {bigIntReplacer, formatError, getQueryID, isValidHexString} from "../utils/utils";
 import {buildInternalMessage} from "./userLock";
 
 export async function buildSmgReleaseMessage(opts: {
@@ -20,19 +20,38 @@ export async function buildSmgReleaseMessage(opts: {
     p: bigint,
     s: bigint,
     queryID?: bigint,
-    bridgeScAddr: string
+    bridgeScAddr: string,
 }) {
+    try {
+        if (!isValidHexString(opts.smgID)) {
+            throw Error("smgId is not valid hex string")
+        }
+        if (!Address.isAddress(opts.userAccount)) {
+            throw Error("userAccount is not valid address")
+        }
 
-    logger.info("buildSmgReleaseMessage", "opts", JSON.stringify(opts, bigIntReplacer));
-    if (!opts.queryID) {
-        opts.queryID = BigInt(await getQueryID());
-    }
-    let body = codeTable[opcodes.OP_CROSS_SmgRelease].enCode(opts);
-    let msg = await buildInternalMessage({to: opts.bridgeScAddr, value: opts.value, body: body, bounce: true});
-    return {
-        internalMsg: msg,
-        body: body,
-        to: opts.bridgeScAddr,
-        value: opts.value,
+        if (!Address.isAddress(opts.bridgeJettonWalletAddr)) {
+            throw Error("bridgeJettonWalletAddr is not valid address")
+        }
+
+        if (!Address.isAddress(Address.parse(opts.bridgeScAddr))) {
+            throw Error("bridgeScAddr is not valid address")
+        }
+
+        logger.info("buildSmgReleaseMessage", "opts", JSON.stringify(opts, bigIntReplacer));
+        if (!opts.queryID) {
+            opts.queryID = BigInt(await getQueryID());
+        }
+        let body = codeTable[opcodes.OP_CROSS_SmgRelease].enCode(opts);
+        let msg = await buildInternalMessage({to: opts.bridgeScAddr, value: opts.value, body: body, bounce: true});
+        return {
+            internalMsg: msg,
+            body: body,
+            to: opts.bridgeScAddr,
+            value: opts.value,
+        }
+    } catch (e) {
+        logger.error(formatError(e));
+        throw formatError(e);
     }
 }
