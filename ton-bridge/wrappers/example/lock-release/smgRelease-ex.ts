60198fceca59f4d5a605ff867dd7f3f7889fd393
Jacob <36022598+13552128609@users.noreply.github.com>
2025-07-07 10:34:47 +0800

    smgRelease support mainnet

--- ton-bridge/wrappers/example/lock-release/smgRelease-ex.ts
+++ ton-bridge/wrappers/example/lock-release/smgRelease-ex.ts
@@ -1,105 +1,121 @@
-import {TonClient} from "@ton/ton";
-import {getClient, TonClientConfig, wanTonSdkInit} from "../../client/client";
+import {getClient, wanTonSdkInit} from "../../client/client";
 import {getSenderByPrvKey, getWalletByPrvKey} from "../../wallet/walletContract";
-import {buildUserLockMessages} from "../../code/userLock";
-import {Address, toNano} from "@ton/core";
+import {Address} from "@ton/core";
 import {BridgeAccess} from "../../contractAccess/bridgeAccess";
-import {getQueryID, sleep} from "../../utils/utils";
+import {getQueryID, sleep, toNumberByDecimal} from "../../utils/utils";
 import {common} from "../../common";
-import {BIP44_CHAINID, TON_COIN_ACCOUNT_STR, TON_COIN_ACCOUT} from "../../const/const-value";
+import {BIP44_CHAINID, TON_COIN_ACCOUNT_STR} from "../../const/const-value";
 
-import {getJettonAddress } from "../../wallet/jetton";
-
-const schnorr = require("../../sign/tools-secp256k1.js");
-import * as util from "node:util";
+import {getJettonAddress} from "../../wallet/jetton";
 import {TON_FEE} from "../../fee/fee";
 
-import {configTestnet,configMainnet} from "../../config/config-ex";
+import {configMainnetNoDb, configTestnetNoDb} from "../../config/config-ex";
 import {WanTonClient} from "../../client/client-interface";
+
+const schnorr = require("../../sign/tools-secp256k1.js");
+const smgConfig = require("../../testData/smg.json");
+
 const prvList = require('../../testData/prvlist')
-const prvAlice = Buffer.from(prvList[1],'hex');
-const prvBob = Buffer.from(prvList[2],'hex');
 
-const scAddresses = require('../../testData/contractAddress.json');
+const optimist = require('optimist');
+let argv = optimist
+    .usage("Usage: $0")
+    .alias('h', 'help')
+    .describe('network', 'network name testnet|mainnet')
+    .describe('tpId', 'token pair Id')
+    .describe('releaseValue', 'releaseValue')
+    .describe('releaseFee', 'releaseFee')
+    .describe('decimal', 'decimal of the lock token or coin')
+    .describe('smgId', 'storeman group Id')
+    .describe('destAddr', 'address of ton address')
+    .string(['smgId'])
+    .argv;
 
-const smgCfg = require('../../testData/smg.json');
-const tokenInfo = require('../../testData/tokenInfo.json');
-let smgID = smgCfg.smgId
-let smgReleaseValue = toNano('0.1')
-let bridgeScAddr = scAddresses.bridgeAddress
-let transValueSmgRelease = TON_FEE.TRANS_FEE_SMG_RELEASE
-let smgAgentFee = BigInt(1000); // should get from contract.
-let dstUserAccount = "0xF6eB3CB4b187d3201AfBF96A38e62367325b29F9"
-let aliceSender;
-
-let smgConfig = require('../../testData/smg.json')
+console.log(optimist.argv);
+console.log("smgId", argv['smgId']);
+console.log("destAddr", argv['destAddr']);
+console.log((Object.getOwnPropertyNames(optimist.argv)).length);
 
 
-let client = null;
-let aliceWallet,aliceAddressStr,aliceAddress;
-let bobWallet,bobAddress,bobAddressStr;
-
-async function init() {
-    //await wanTonSdkInit(configMainnet);
-    await wanTonSdkInit(configTestnet);
-    client = await getClient();
-    aliceWallet = await getWalletByPrvKey(prvAlice);
-    bobWallet = await getWalletByPrvKey(prvBob);
-
-    aliceAddressStr = aliceWallet.address.toString();
-    aliceAddress = aliceWallet.address;
-
-    bobAddressStr = bobWallet.address.toString();
-    bobAddress = bobWallet.address;
-    aliceSender = await getSenderByPrvKey(client, prvAlice);
+if ((Object.getOwnPropertyNames(optimist.argv)).length < 6) {
+    optimist.showHelp();
+    process.exit(0);
 }
 
+global.network = argv["network"];
+const config = require('../../config/config');
 
-async function getAgentFee(tokenPairID:number,srcChainId:number,dstChainId:number){
-    let ba = BridgeAccess.create(client,bridgeScAddr);
-    let tokenPairFee = await ba.readContract('getTokenPairFee',[tokenPairID]);
-    let chainFee = await ba.readContract('getChainFee',[srcChainId,dstChainId]);
-    if(chainFee.agentFee){
+let client = null;
+
+const scAddresses = require(config.contractOutput);
+
+let releaseValue = toNumberByDecimal(argv['releaseValue'], argv['decimal'])
+let releaseFee = toNumberByDecimal(argv['releaseFee'], argv['decimal'])
+let bridgeScAddr = scAddresses.bridgeAddress
+let aliceSender;
+let aliceWallet, bobWallet, bobAddress;
+
+async function init() {
+
+    if (global.network == 'testnet') {
+        await wanTonSdkInit(configTestnetNoDb);
+    } else {
+        await wanTonSdkInit(configMainnetNoDb);
+    }
+
+    client = await getClient();
+    aliceWallet = await getWalletByPrvKey(Buffer.from(prvList[0], 'hex'));
+    bobWallet = await getWalletByPrvKey(Buffer.from(prvList[1], 'hex'));
+    bobAddress = bobWallet.address;
+    aliceSender = await getSenderByPrvKey(client, Buffer.from(prvList[0], 'hex'));
+}
+
+async function getAgentFee(tokenPairID: number, srcChainId: number, dstChainId: number) {
+    let ba = BridgeAccess.create(client, bridgeScAddr);
+    let tokenPairFee = await ba.readContract('getTokenPairFee', [tokenPairID]);
+    let chainFee = await ba.readContract('getChainFee', [srcChainId, dstChainId]);
+    if (chainFee.agentFee) {
         return chainFee.agentFee
-    }else{
+    } else {
         return tokenPairFee.agentFee
     }
 }
-const jetton = require('../../wallet/jetton');
 
-async function getJettonWalletAddr(client:WanTonClient,tokenAccountAddr:Address,ownerAddress:Address){
-    if(tokenAccountAddr.equals(Address.parse(TON_COIN_ACCOUNT_STR))){
+async function getJettonWalletAddr(client: WanTonClient, tokenAccountAddr: Address, ownerAddress: Address) {
+    if (tokenAccountAddr.equals(Address.parse(TON_COIN_ACCOUNT_STR))) {
         return Address.parse(TON_COIN_ACCOUNT_STR);
-    }else{
-        return getJettonAddress(client,tokenAccountAddr,ownerAddress);
+    } else {
+        return getJettonAddress(client, tokenAccountAddr, ownerAddress);
     }
 }
 
-async function buildSmgReleaseParameters(client:WanTonClient,input:{
-    smgID:string,
-    tokenPairID:number,
-    releaseValue:bigint,
-    value:bigint,
-    queryID:bigint,
-    uniqueID:bigint,
-    tokenCoinAccount:Address,
-    destAccount:Address,
-    fwTonAmount:bigint,
-    totalTonAmount:bigint,
-},srcChainId:number,dstChainId:number,bridgeScAddr:Address){
-    let bridgeJwAddress = await getJettonWalletAddr(client,input.tokenCoinAccount, bridgeScAddr);
-    let agentFee = await getAgentFee(input.tokenPairID,srcChainId,dstChainId);
+async function buildSmgReleaseParameters(client: WanTonClient, input: {
+    smgID: string,
+    tokenPairID: number,
+    releaseValue: bigint,
+    releaseFee: bigint,
+    value: bigint,
+    queryID: bigint,
+    uniqueID: bigint,
+    tokenCoinAccount: Address,
+    destAccount: Address,
+    fwTonAmount: bigint,
+    totalTonAmount: bigint,
+    bridgeScAddr: Address
+}) {
+    let bridgeJwAddress = await getJettonWalletAddr(client, input.tokenCoinAccount, input.bridgeScAddr);
+
 
     let msgHashResult = common.computeHash(BigInt(BIP44_CHAINID),
         BigInt(input.uniqueID),
         BigInt(input.tokenPairID),
         BigInt(input.releaseValue),
-        BigInt(agentFee),
+        BigInt(releaseFee),
         input.tokenCoinAccount,
         input.destAccount);
-    console.log("smgConfig.skSmg=>",smgConfig.skSmg);
-    let sig = schnorr.getSecSchnorrSByMsgHash(Buffer.from(smgConfig.skSmg,'hex'),msgHashResult.hashBuf);
-    console.log("sig=>",sig);
+    console.log("smgConfig.skSmg=>", smgConfig.skSmg);
+    let sig = schnorr.getSecSchnorrSByMsgHash(Buffer.from(smgConfig.skSmg, 'hex'), msgHashResult.hashBuf);
+    console.log("sig=>", sig);
     const e = BigInt(sig.e);
 
     const p = BigInt(sig.p);
@@ -107,74 +123,64 @@ async function buildSmgReleaseParameters(client:WanTonClient,input:{
 
     return {
         value: input.value,
-        queryID:input.queryID,
-        uniqueID:input.uniqueID,
-        smgID:input.smgID,
-        tokenPairID:input.tokenPairID,
-        releaseValue:input.releaseValue,
-        fee:agentFee,
+        queryID: input.queryID,
+        uniqueID: input.uniqueID,
+        smgID: input.smgID,
+        tokenPairID: input.tokenPairID,
+        releaseValue: input.releaseValue,
+        fee: releaseFee,
         userAccount: input.destAccount,
-        bridgeJettonWalletAddr:bridgeJwAddress,
+        bridgeJettonWalletAddr: bridgeJwAddress,
         e,
         p,
         s,
-        fwTonAmount:input.fwTonAmount,
-        totalTonAmount:input.totalTonAmount,
+        fwTonAmount: input.fwTonAmount,
+        totalTonAmount: input.totalTonAmount,
     }
 }
 
-async function smgRelease(){
-    try{
-        let transValueSmg : bigint = transValueSmgRelease;
-        let ba = BridgeAccess.create(client,bridgeScAddr);
-        for(let key of Object.keys(tokenInfo)) {
-            try{
-                console.log("key:",key);
-                // if(key.toString().toLowerCase() !== "tokenwrapped"){
-                //     continue;
-                // }
-                // if(key.toString().toLowerCase() !== "coin"){
-                //     continue;
-                // }
-                // if(key.toString().toLowerCase() !== "tokenOrg".toLowerCase()){
-                //     continue;
-                // }
 
-                if(key.toString().toLowerCase() !== "ton".toLowerCase()){
-                    transValueSmg = TON_FEE.TRANS_FEE_USER_LOCK_TOKEN;
-                }
+async function smgRelease() {
+    console.log("Entering smgRelease", "tpId", argv['tpId']);
+    let ba = BridgeAccess.create(client, bridgeScAddr.toString());
+    console.log("bridgeScAddr", bridgeScAddr.toString());
+    let retTp = await ba.readContract("getTokenPair", [argv['tpId']])
+    let transValueSmg: bigint = TON_FEE.TRANS_FEE_SMG_RELEASE;
 
-                let smgReleasePara = await buildSmgReleaseParameters(client,{
-                    smgID:smgConfig.smgId,
-                    tokenPairID:tokenInfo[key].tokenPairId,
-                    releaseValue:smgReleaseValue,
-                    value:transValueSmg,
-                    queryID:BigInt(await getQueryID()),
-                    uniqueID:BigInt(await getQueryID()),  // should be txHas->bigInt, here is the example.
-                    tokenCoinAccount:Address.parse(tokenInfo[key].dstTokenAcc),
-                    //tokenCoinAccount:Address.parseFriendly(tokenInfo.tokenOrg.dstTokenAcc).address,
-                    destAccount:bobAddress,
-                    fwTonAmount:TON_FEE.FWD_TON_AMOUNT_TRANSFER_JETTON,
-                    totalTonAmount:TON_FEE.TOTAL_TON_AMOUNT_TRANSFER_JETTON
-                },tokenInfo[key].srcChainId,tokenInfo[key].dstChainId,Address.parse(bridgeScAddr))
-                let ret = await ba.writeContract('sendSmgRelease', aliceSender, smgReleasePara)
-                await sleep(3000);
-                console.log("key = %s, ret of smRelease is %s",key,ret);
-            }catch(err){
-                console.error("key = %s, error: %s",key,err);
-            }
+    try {
+        let smgReleasePara = await buildSmgReleaseParameters(client, {
+            smgID: smgConfig.smgId,
+            tokenPairID: argv['tpId'],
+            releaseValue: releaseValue,
+            releaseFee,
+            value: transValueSmg,
+            queryID: BigInt(await getQueryID()),
+            uniqueID: BigInt(await getQueryID()),  // should be txHas->bigInt, here is the example.
+            tokenCoinAccount: Address.parse(retTp.tokenAccount),
+            destAccount: Address.parse(argv['destAddr']),
+            fwTonAmount: TON_FEE.FWD_TON_AMOUNT_TRANSFER_JETTON,
+            totalTonAmount: TON_FEE.TOTAL_TON_AMOUNT_TRANSFER_JETTON,
+            bridgeScAddr: Address.parse(bridgeScAddr),
+        })
+        let ret = await ba.writeContract('sendSmgRelease', aliceSender, smgReleasePara)
+        await sleep(3000);
+        console.log("ret", ret);
 
-            await sleep(3000);
-        }
-
-    }catch(e){
-        console.log("err  =%s",e.Error,e);
+    } catch (err) {
+        console.error("error: %s", err);
+        await sleep(3000);
     }
+
+
 }
+
 async function main() {
 
     await init();
     await smgRelease();
 
 }
+
 main();
+
+//ts-node smgRelease-ex.ts --network testnet --tpId 1030 --releaseValue 100 --releaseFee 10 --decimal 6 --smgId 0x000000000000000000000000000000000000000000746573746e65745f303638 --destAddr EQCGOHmrNm3u_ilZ5qdtpIDmfVfkQsWsqxyvPywT_7_fOzZh
--- ton-bridge/wrappers/fee/fee.ts
+++ ton-bridge/wrappers/fee/fee.ts
@@ -1,7 +1,8 @@
 //
 import {toNano} from "@ton/core";
+
 export const TON_FEE = {
-    TRANS_FEE_NORMAL : toNano('0.1'),
+    TRANS_FEE_NORMAL: toNano('0.1'),
 
     // for mint token
     FWD_FEE_MINT_JETTON: toNano('0.01'),
@@ -11,13 +12,13 @@ export const TON_FEE = {
     TRANS_FEE_DEPLOY: toNano('0.1'),
 
     // for user lock
-    TRANS_FEE_USER_LOCK_TOKEN:toNano('1'),
-    NOTIFY_FEE_USER_LOCK:toNano('0.8'),
+    TRANS_FEE_USER_LOCK_TOKEN: toNano('1'),
+    NOTIFY_FEE_USER_LOCK: toNano('0.8'),
 
     // for smg release
-    TRANS_FEE_SMG_RELEASE: toNano('0.3'),
-    TRANS_FEE_SMG_RELEASE_TOKEN:toNano('0.5'),        // for smgRelease, the first tx value
-    FWD_TON_AMOUNT_TRANSFER_JETTON:toNano('0.0'),     // for smgRelease , send token
-    TOTAL_TON_AMOUNT_TRANSFER_JETTON:toNano('0.2'),   // for smgRelease , send token
+    TRANS_FEE_SMG_RELEASE: toNano('1'),
+    TRANS_FEE_SMG_RELEASE_TOKEN: toNano('0.5'),        // for smgRelease, the first tx value
+    FWD_TON_AMOUNT_TRANSFER_JETTON: toNano('0.0'),     // for smgRelease , send token
+    TOTAL_TON_AMOUNT_TRANSFER_JETTON: toNano('0.2'),   // for smgRelease , send token
 }
 
